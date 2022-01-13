6903656ff955542b282aa757b4d64de7a951f511
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-01-13 17:09:33 +0000

    perros sub

--- monty.c
+++ monty.c
@@ -47,7 +47,8 @@ void is_opt(char *buffer, stack_t **head, unsigned int line)
 
 	instruction_t opts[] = { {"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop},
 							 {"swap", swap}, {"add", add}, {"nop", nop},
-							 {"sub", sub}, {"div", divi}, {NULL, NULL}};
+							 {"sub", sub}, {"div", divi}, {"mul", mul},
+							  {NULL, NULL}};
 	op = strtok(buffer, " \t \n");
 	while (opts[idx].opcode)
 	{
--- more_funtions2.c
+++ more_funtions2.c
@@ -52,3 +52,27 @@ void divi(stack_t **stack, unsigned int line_number)
         exit(EXIT_FAILURE);
     }
 }
+
+/**
+ * mul - muls the top two elements of the stack..
+ * @stack: double linked list
+ * @line_number: number of evaluated line
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+    stack_t *temp;
+
+    if (*stack && (*stack)->next)
+    {
+        temp = (*stack)->next;
+        (*stack)->next->n = (*stack)->next->n * (*stack)->n;
+        pop(stack, line_number);
+        *stack = temp;
+    }
+    else
+    {
+        fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+        fclose(red);
+        exit(EXIT_FAILURE);
+    }
+}
